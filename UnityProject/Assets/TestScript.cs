55880e8817a1046e29fdcc17d2005f6c71969d39
unknown <localadmin@panama.cs.ucl.ac.uk>
2014-11-26 15:20:42 +0000

    applying fix from matlab to unity. Also making unity output points to .m file so matlab can be using exactly the same stuff (for debugging)

--- UnityProject/Assets/TestScript.cs
+++ UnityProject/Assets/TestScript.cs
@@ -4,6 +4,7 @@ using OpenCvSharp;
 using OpenCvSharp.CPlusPlus;
 using System.Collections.Generic;
 using System.Text;
+using System.IO;
 
 public class TestScript : MonoBehaviour {
 
@@ -13,8 +14,10 @@ public class TestScript : MonoBehaviour {
 
     public void calibrateFromCorrespondences(List<Vector3> _imagePositions, List<Vector3> _objectPositions, bool usingNormalized)
     {
-        double height = (double) Screen.height;
-        double width = (double) Screen.width;
+        StringBuilder sb = new StringBuilder();
+
+        double height = (double)Screen.height;
+        double width = (double)Screen.width;
         int pointsCount = _imagePositions.Count;
         int ImageNum = 1;
         int[] pointCountsValue = new int[ImageNum];
@@ -22,6 +25,7 @@ public class TestScript : MonoBehaviour {
 
         CvPoint3D32f[,] objects = new CvPoint3D32f[ImageNum, pointsCount];
 
+        sb.Append("objectPoints = [");
         for (int i = 0; i < pointsCount; i++)
         {
             objects[0, i] = new CvPoint3D32f
@@ -30,14 +34,20 @@ public class TestScript : MonoBehaviour {
                 Y = _objectPositions[i].y,
                 Z = _objectPositions[i].z * -1
             };
+            sb.AppendLine(_objectPositions[i].x + ", " + _objectPositions[i].y + ", " + _objectPositions[i].z + ";");
         }
+        sb.AppendLine("]';");
 
         CvMat objectPoints = new CvMat(pointsCount, 3, MatrixType.F32C1, objects);
         List<CvPoint2D32f> allCorners = new List<CvPoint2D32f>(pointsCount);
+        sb.Append("imagePoints = [");
         for (int i = 0; i < _imagePositions.Count; i++)
         {
             allCorners.Add(new CvPoint2D32f(_imagePositions[i].x, _imagePositions[i].y));
+            sb.AppendLine(_imagePositions[i].x + ", " + _imagePositions[i].y + ";");
         }
+        sb.AppendLine("]';");
+
 
         CvMat imagePoints = new CvMat(pointsCount, 1, MatrixType.F32C2, allCorners.ToArray());
         CvMat pointCounts = new CvMat(ImageNum, 1, MatrixType.S32C1, pointCountsValue);
@@ -53,7 +63,7 @@ public class TestScript : MonoBehaviour {
             //double fy = 37.469987986050846;
 
             float fov = 60.0f;
-            float focalLength = (float) height / (2.0f * Mathf.Tan(0.5f * fov * Mathf.Deg2Rad));
+            float focalLength = (float)height / (2.0f * Mathf.Tan(0.5f * fov * Mathf.Deg2Rad));
 
             double fx = focalLength;
             double fy = focalLength;
@@ -74,38 +84,55 @@ public class TestScript : MonoBehaviour {
             0.0, fy, cy, 
             0.0, 0.0, 1.0 };
             intrinsic = new CvMat(3, 3, MatrixType.F64C1, intrGuess);
+
+            sb.AppendLine("fc = [" + focalLength + ", " + focalLength + "];");
+            sb.AppendLine(" cc = [" + cx + ", " + cy + "];");
         }
         else
         {
             intrinsic = prevIntrinsic;
         }
 
+        using (StreamWriter outfile = new StreamWriter("pointData.m"))
+        {
+            outfile.Write(sb.ToString());
+        }
+
         CvMat distortion = new CvMat(1, 4, MatrixType.F64C1);
         CvMat rotation = new CvMat(ImageNum, 3, MatrixType.F64C1);
         CvMat translation = new CvMat(ImageNum, 3, MatrixType.F64C1);
 
-        Cv.CalibrateCamera2(objectPoints, imagePoints, pointCounts, size, intrinsic, distortion, rotation, translation, CalibrationFlag.UseIntrinsicGuess | CalibrationFlag.ZeroTangentDist);
+        Cv.CalibrateCamera2(objectPoints, imagePoints, pointCounts, size, intrinsic, distortion, rotation, translation, CalibrationFlag.FixIntrinsic | CalibrationFlag.UseIntrinsicGuess );
         prevIntrinsic = intrinsic;
 
-       CvMat rotation_ = new CvMat(1, 3, MatrixType.F32C1);
-       CvMat translation_ = new CvMat(1, 3, MatrixType.F32C1);
+        CvMat rotation_ = new CvMat(1, 3, MatrixType.F32C1);
+        CvMat translation_ = new CvMat(1, 3, MatrixType.F32C1);
 
-       translation_[0, 0] = _mainCamera.transform.position.x;
-       translation_[0, 1] = _mainCamera.transform.position.y;
-       translation_[0, 2] = _mainCamera.transform.position.z;
-
-       Cv.FindExtrinsicCameraParams2(objectPoints, imagePoints, intrinsic, distortion, rotation_, translation_, true);
+        Cv.FindExtrinsicCameraParams2(objectPoints, imagePoints, intrinsic, distortion, rotation_, translation_, false);
 
         // NB code is mostly from:
         // https://github.com/shimat/opencvsharp/blob/master/sample/CStyleSamplesCS/Samples/CalibrateCamera.cs
 
-        CvMat rotInv = new CvMat(3, 1, MatrixType.F32C1);
+        //CvMat rotInv = new CvMat(3, 3, MatrixType.F32C1);
+        //rotationFull.Invert(rotInv as CvArr);
 
-        rotation_.Invert(rotInv as CvArr);
+        CvMat rotationFull = new CvMat(3, 3, MatrixType.F32C1);
+        Cv.Rodrigues2(rotation_, rotationFull);
 
-        double x = translation[0, 0];
-        double y = translation[0, 1];
-        double z = translation[0, 2];
+        float[] LHSflipBackMatrix = new float[] { 1.0f, 0.0f, 0.0f, 
+            0.0f, 1.0f, 0.0f, 
+            0.0f, 0.0f, -1.0f };
+        CvMat LHSflipBackMatrixM = new CvMat(3, 3, MatrixType.F32C1, LHSflipBackMatrix);
+        CvMat rotLHS = rotationFull * LHSflipBackMatrixM; // invert Z (as we did before when savings points) to get from RHS -> LHS
+
+        CvMat rotTran = rotLHS.Transpose();
+        CvMat transTran = translation_.Transpose();
+
+        CvMat transFinal = (rotTran * -1) * transTran;
+
+        double x = transFinal[0, 0];
+        double y = transFinal[0, 1];
+        double z = transFinal[0, 2];
 
         double rx = rotation[0, 0];
         double ry = rotation[0, 1];
@@ -116,7 +143,8 @@ public class TestScript : MonoBehaviour {
         //projectorCamera.transform.eulerAngles = new Vector3((float)rx, (float)ry, (float)rz);
         //.Rotate(new Vector3((float)rx, (float)ry, (float)rz), Space.World);
 
-	}
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
