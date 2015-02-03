bff0b039d61d9e3755bef136cf87591d3c620e04
andrewmacquarrie <andrew.macquarrie@gmail.com>
2015-02-03 17:24:57 +0000

    adding Unity projector. Projects light onto cube from given viewpoint - creates perspective correct rendering. Added shader to unlight projection surface

---
+++ UnityProject/Assets/TextureCube.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TextureCube : MonoBehaviour {
+
+	// Use this for initialization
+    void Start()
+    {
+
+        MeshFilter mf = GetComponent<MeshFilter>();
+        Mesh mesh = null;
+        if (mf != null)
+        {
+            mesh = mf.mesh;
+            if (mesh == null || mesh.uv.Length != 24)
+            {
+                Debug.Log("Script needs to be attached to built-in cube");
+                return;
+            }
+        }
+
+        if (mesh == null)
+            return;
+
+        var uvs = mesh.uv;
+
+        // Front
+        uvs[0] = new Vector2(0.0f, 0.0f);
+        uvs[1] = new Vector2(0.333f, 0.0f);
+        uvs[2] = new Vector2(0.0f, 0.333f);
+        uvs[3] = new Vector2(0.333f, 0.333f);
+
+        // Top
+        uvs[8] = new Vector2(0.334f, 0.0f);
+        uvs[9] = new Vector2(0.666f, 0.0f);
+        uvs[4] = new Vector2(0.334f, 0.333f);
+        uvs[5] = new Vector2(0.666f, 0.333f);
+
+        // Back
+        uvs[10] = new Vector2(0.667f, 0.0f);
+        uvs[11] = new Vector2(1.0f, 0.0f);
+        uvs[6] = new Vector2(0.667f, 0.333f);
+        uvs[7] = new Vector2(1.0f, 0.333f);
+
+        // Bottom
+        uvs[12] = new Vector2(0.0f, 0.334f);
+        uvs[14] = new Vector2(0.333f, 0.334f);
+        uvs[15] = new Vector2(0.0f, 0.666f);
+        uvs[13] = new Vector2(0.333f, 0.666f);
+
+        // Left
+        uvs[16] = new Vector2(0.334f, 0.334f);
+        uvs[18] = new Vector2(0.666f, 0.334f);
+        uvs[19] = new Vector2(0.334f, 0.666f);
+        uvs[17] = new Vector2(0.666f, 0.666f);
+
+        // Right        
+        uvs[20] = new Vector2(0.667f, 0.334f);
+        uvs[22] = new Vector2(1.00f, 0.334f);
+        uvs[23] = new Vector2(0.667f, 0.666f);
+        uvs[21] = new Vector2(1.0f, 0.666f);
+
+        mesh.uv = uvs;
+    }
+
+    IEnumerator Fade()
+    {
+        for (float f = 1f; f <= 180; f += 1f)
+        {
+            this.transform.Rotate(new Vector3(0.0f, 1.0f, 0.0f), 1.0f);
+            yield return new WaitForSeconds(.001f);
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown("s"))
+        {
+            StartCoroutine("Fade");
+        }
+    }
+}
+
---
+++ UnityProject/Assets/Unlit.shader
@@ -0,0 +1,62 @@
+﻿ Shader "Projector/Unlit"
+ {
+   // http://answers.unity3d.com/questions/40357/projector-projector-shader-and-cg.html
+     Properties
+     {
+         _ShadowTex ("Cookie", 2D) = "gray" { TexGen ObjectLinear }
+          _FalloffTex ("FallOff", 2D) = "white" { TexGen ObjectLinear   }
+     }
+      
+     Subshader
+     {
+         Tags { "RenderType"="Transparent-1" }
+         Pass
+         {
+             //ZWrite Off
+              Fog { Color (1, 1, 1) }
+              AlphaTest Less 1
+              ColorMask RGB
+              //Blend DstColor Zero
+              Offset -1, -1
+                          
+             CGPROGRAM
+             #pragma vertex vert
+             #pragma fragment frag
+             #pragma fragmentoption ARB_fog_exp2
+             #pragma fragmentoption ARB_precision_hint_fastest
+             #include "UnityCG.cginc"
+              
+             struct v2f
+             {
+                 float4 pos : SV_POSITION;
+                 float4 uv_Main : TEXCOORD0;
+                 float4 uv_Clip : TEXCOORD1;
+             };
+             
+              
+             sampler2D _ShadowTex;
+             sampler2D _FalloffTex;
+             float4x4 _Projector;
+             float4x4 _ProjectorClip;
+              
+             v2f vert(appdata_tan v)
+             {
+                 v2f o;
+                 o.pos = mul (UNITY_MATRIX_MVP, v.vertex);
+                 o.uv_Main = mul (_Projector, v.vertex);
+                 o.uv_Clip = mul (_ProjectorClip, v.vertex);
+                 return o;
+             }
+              
+             half4 frag (v2f i) : COLOR
+             {
+                 half4 tex = tex2Dproj(_ShadowTex, i.uv_Main);
+                 half4 falloff = tex2D(_FalloffTex, i.uv_Clip.xy);
+                 tex = lerp(float4(1,1,1,1),tex,falloff.a);
+                 return tex;
+             }
+             ENDCG
+      
+         }
+     }
+ }
